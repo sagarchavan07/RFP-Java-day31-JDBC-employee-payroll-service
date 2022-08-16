73746281fe07cb38b821a342732e3e6c1509e3a0
sagarchavan07 <chavansagar96044@gmail.com>
2022-08-16 21:36:09 +0530

    retrive employee_payroll data from database

---
+++ src/main/java/EmployeePayrollException.java
@@ -0,0 +1,8 @@
+public class EmployeePayrollException extends Exception{
+    public EmployeePayrollException() {
+    }
+
+    public EmployeePayrollException(String message) {
+        super(message);
+    }
+}
