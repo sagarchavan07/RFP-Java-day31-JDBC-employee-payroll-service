d6fbecfa332861f0164287594c831dc20f2ee866
sagarchavan07 <chavansagar96044@gmail.com>
2022-08-16 20:14:05 +0530

    create connection with database

--- src/main/java/JDBCExample.java
+++ src/main/java/JDBCExample.java
@@ -1,5 +1,7 @@
 import java.sql.Connection;
+import java.sql.Driver;
 import java.sql.DriverManager;
+import java.util.Enumeration;
 
 public class JDBCExample {
     public static void main(String[] args) {
@@ -9,12 +11,27 @@ public class JDBCExample {
         Connection connection;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection(URL, USER, PASS);
-            connection.close();
+            System.out.println("Driver Loaded!");
         } catch (ClassNotFoundException e) {
-            System.out.println("ClassNotFound Exception");
+            System.out.println("can't find the driver in the classpath!");
+        }
+
+        listDrivers();
+        try {
+            System.out.println("connecting to database: "+URL);
+            connection = DriverManager.getConnection(URL, USER, PASS);
+            System.out.println("connection is successful! "+connection);
+            connection.close();
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void listDrivers(){
+        Enumeration<Driver> driverList=DriverManager.getDrivers();
+        while (driverList.hasMoreElements()){
+            Driver driverClass=driverList.nextElement();
+            System.out.println(driverClass.getClass().getName()+" ");
         }
     }
 }
